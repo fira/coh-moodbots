79454b369ffce39bfa96b0a1fd7f4b9a8bd1246f
Fira <loyauflorian@gmail.com>
2025-03-26 21:10:40 +0000

    editorconfig + bugfix

--- generator.rb
+++ generator.rb
@@ -69,96 +69,103 @@ sampled_names = []
 for bfi in 0..bf_total
   # Start by selecting a mood if needed
   if bfi >= mood_next
-    announce_mood = true
-    mood_data = behaviors.sample
-    mood = mood_data[0]
-    cycle_prob = rand(bf_edgy_prob) * mood_data[1]
-    mood_next = bfi + rand(bf_behavior_cycle) * mood_data[2]
-    context = []
+	announce_mood = true
+	mood_data = behaviors.sample
+	mood = mood_data[0]
+	cycle_prob = rand(bf_edgy_prob) * mood_data[1]
+	mood_next = bfi + rand(bf_behavior_cycle) * mood_data[2]
+	context = []
 
-    # Now read the beahvior data
-    mood_opts = Hash.new
-    announce_opts = Hash.new
-    command = [] # Set of sequential commands for several actions in a row
-    commandset.each do |key, name, default|
-      mood_opts[name] = []
-      announce_opts[name] = []
-      File.foreach("behaviors/#{mood}/#{name}") do |line|
-        linecmd = []
-        # First character are special ops (+: add to default, >: next line is direct follow-up)
-        spl = line.split(' ')
-        if /\@/ =~ spl.first
-          linecmd.push("petsay_name %R %@")
-          linecmd.push(terminal_mischief.sample)
-        else
-          if /\+/ =~ spl.first
-            linecmd.push(default)
-          end
-          linecmd.push(spl[1..].join(' '))
-        end
-        command.push(linecmd)
-        if not />/ =~ spl.first
-          mood_opts[name].push(command)
-          command = []
-        end
-      end
-      File.foreach("behaviors/#{mood}/announce") do |line|
-        linecmd = []
-        # First character are special ops (+: add to default, >: next line is direct follow-up)
-        spl = line.split(' ')
-        if /\+/ =~ spl.first
-          linecmd.push(default)
-        end
-        linecmd.push(spl[1..].join(' '))
-        command.push(linecmd)
-        if not />/ =~ spl.first
-          announce_opts[name].push(command)
-          command = []
-        end
-      end
-    end
+	# Now read the beahvior data
+	mood_opts = Hash.new
+	announce_opts = Hash.new
+	command = [] # Set of sequential commands for several actions in a row
+	commandset.each do |key, name, default|
+	  mood_opts[name] = []
+	  announce_opts[name] = []
+	  File.foreach("behaviors/#{mood}/#{name}") do |line|
+		if line.empty?
+			next
+		end
+		linecmd = []
+		# First character are special ops (+: add to default, >: next line is direct follow-up)
+		spl = line.split(' ')
+		if /\@/ =~ spl.first
+		  linecmd.push("petsay_name %R %@W")
+		  linecmd.push("%@C")
+		else
+		  if /\+/ =~ spl.first
+			linecmd.push(default)
+		  end
+		  linecmd.push(spl[1..].join(' '))
+		end
+		command.push(linecmd)
+		if not />/ =~ spl.first
+		  mood_opts[name].push(command)
+		  command = []
+		end
+	  end
+	  File.foreach("behaviors/#{mood}/announce") do |line|
+	  	if line.empty?
+			next
+		end
+		linecmd = []
+		# First character are special ops (+: add to default, >: next line is direct follow-up)
+		spl = line.split(' ')
+		if /\+/ =~ spl.first
+		  linecmd.push(default)
+		end
+		linecmd.push(spl[1..].join(' '))
+		command.push(linecmd)
+		if not />/ =~ spl.first
+		  announce_opts[name].push(command)
+		  command = []
+		end
+	  end
+	end
   end
 
   # Next generate a bind file according to mood
   target_filename = bfi.to_s.rjust(6, "0")
   nextbf = bfi + 1
   if nextbf > bf_total
-    nextbf = 0
+	nextbf = 0
   end
   target_filename_next = nextbf.to_s.rjust(6, "0")
 
   File.open("#{outdir}/#{subdir}/#{target_filename}", "w") do |fd|
-    commandset.each do |key, name, default|
-      cmdset = [default]
-      if announce_mood or (rand(1000) < cycle_prob and delay_count < 1)
-        if announce_mood
-          context = announce_opts[name].sample.clone
-          sampled_names = []
-        elsif context.empty?
-          context = mood_opts[name].sample.clone
-          sampled_names = []
-        end
-        if not context.empty?
-          cmdset = context.shift.clone
-        end
-        puts cmdset
-        delay_count = bf_delay
-      elsif delay_count > 0
-        delay_count = delay_count - 1
-      end
-      if sampled_names.empty?
-        sampled_names = pet_names.sample(3)
-      end
-      cmdset.push("bindloadfilesilent #{subdir}/#{target_filename_next}")
-      cmdtext = cmdset.join("$$")
-      cmdtext.gsub!("%R",  sampled_names[0])
-      cmdtext.gsub!("%O1", sampled_names[1])
-      cmdtext.gsub!("%O2", sampled_names[2])
-      cmdtext.gsub!("%P", player_name)
-      cryptic = (0...60).map { ['#','!','@', '1', '?', '!', '_', '0', '.'].to_a[rand(9)] }.join
-      cmdtext.gsub!("%@", cryptic)
-      fd.write("#{key} \"#{cmdtext}\"\n")
-    end
+	commandset.each do |key, name, default|
+	  cmdset = [default]
+	  if announce_mood or (rand(1000) < cycle_prob and delay_count < 1)
+		if announce_mood
+		  context = announce_opts[name].sample.clone
+		  sampled_names = []
+		elsif context.empty?
+		  context = mood_opts[name].sample.clone
+		  sampled_names = []
+		end
+		if not context.empty?
+		  cmdset = context.shift.clone
+		end
+		puts cmdset
+		delay_count = bf_delay
+	  elsif delay_count > 0
+		delay_count = delay_count - 1
+	  end
+	  if sampled_names.empty?
+		sampled_names = pet_names.sample(3)
+	  end
+	  cmdset.push("bindloadfilesilent #{subdir}/#{target_filename_next}")
+	  cmdtext = cmdset.join("$$")
+	  cmdtext.gsub!("%R",  sampled_names[0])
+	  cmdtext.gsub!("%O1", sampled_names[1])
+	  cmdtext.gsub!("%O2", sampled_names[2])
+	  cmdtext.gsub!("%P", player_name)
+	  cryptic = (0...60).map { ['#','!','@', '1', '?', '!', '_', '0', '.'].to_a[rand(9)] }.join
+	  cmdtext.gsub!("%@W", cryptic)
+	  cmdtext.gsub!("%@C", terminal_mischief.sample)
+	  fd.write("#{key} \"#{cmdtext}\"\n")
+	end
   end
 
   announce_mood = false
